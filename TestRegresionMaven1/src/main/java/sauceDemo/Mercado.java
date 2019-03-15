544622f77af566edfb453a70ff9e84086cc21ffa
tsoftSelenium <47462319+tsoftSelenium@users.noreply.github.com>
2019-03-15 09:43:21 -0300

    esta va

--- TestRegresionMaven1/src/main/java/sauceDemo/Mercado.java
+++ TestRegresionMaven1/src/main/java/sauceDemo/Mercado.java
@@ -18,16 +18,16 @@ public class Mercado {
 		WebElement continuar = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[2]/a[2]"));
 		continuar.click();		
 		
-		WebElement input = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/input[1]"));
+		WebElement input = driver.findElement(By.xpath("//*[@id=\"first-name\"]"));
 		input.sendKeys("RiverCampeon");
 		
-		input = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/input[2]"));
+		input = driver.findElement(By.xpath("//*[@id=\"last-name\"]"));
 		input.sendKeys("CopaLibertadores");
 		
-		input = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/input[3]"));	
+		input = driver.findElement(By.xpath("//*[@id=\"postal-code\"]"));	
 		input.sendKeys("2018");
 		
-		continuar = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/input[4]"));
+		continuar = driver.findElement(By.xpath("//*[@id=\"checkout_info_container\"]/div/form/div[2]/input]"));
 		continuar.click();
 		
 		continuar = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[8]/a[2]"));
